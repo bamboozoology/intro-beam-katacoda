1230e17fa35741237878c7a9d1744e6b73c5f609
brucearctor <iambruceactor@gmail.com>
2020-03-01 17:09:30 -0800

    initial stuffs

---
+++ src/main/java/org/bamboozoology/strata2020sj/learning/kata/coretransforms/map/pardo/kata_pardo_solution.java
@@ -0,0 +1,50 @@
+package org.bamboozoology.strata2020sj.learning.kata.coretransforms.map.pardo;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.transforms.ToString;
+
+public class kata_pardo_solution {
+
+  public static void main(String[] args) {
+    PipelineOptions options = PipelineOptionsFactory.fromArgs(args).create();
+    Pipeline pipeline = Pipeline.create(options);
+
+    PCollection<Integer> numbers =
+        pipeline.apply(Create.of(1, 2, 3, 4, 5));
+
+    PCollection<Integer> transformed = applyTransform(numbers);
+
+    PCollection<String> output = transformed.apply(ToString.elements());
+
+    output.apply(TextIO.write()
+      .to("multiply_by_ten_pardo")    
+      .withSuffix(".txt")
+      .withNumShards(1)
+    );
+
+    pipeline.run();
+  }
+
+  static PCollection<Integer> applyTransform(PCollection<Integer> input) {
+    return input.apply(ParDo.of(new DoFn<Integer, Integer>() {
+
+      @ProcessElement
+      public void processElement(@Element Integer number, OutputReceiver<Integer> out) {
+        out.output(number * 10);
+      }
+
+    }));
+  }
+
+}
+
+
+
+
---
+++ testing/check_multiply_by_ten_pardo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+#CHECKING ANSWERS FOR MULTIPLY BY 10 PARDO
+
+def check_multiply_by_ten_pardo():
+	set_of_answers = set(['10','20','30','40','50'])
+	#print(set_of_answers)
+	set_in_file = set()
+
+	with open('../multiply_by_ten_pardo-00000-of-00001.txt') as f:
+		for line in f:
+			set_in_file.add(line.strip())
+
+	exit(0) if set_of_answers == set_in_file else exit(1)
+
+check_multiply_by_ten_pardo()
